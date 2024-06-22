2a0159f6698c4ec1f2777f8605f363ad8497d3c7
seokhochoi <seokho9473@gmail.com>
2024-06-22 18:51:19 +0900

    style #18: color 관련 오탈자 수정

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,20 +1,20 @@
-import EyeShow from "@/assets/icons/eye_show.svg";
-import EyeHide from "@/assets/icons/eye_hide.svg";
-import AI from "@/assets/icons/ai.svg";
-import Arrow from "@/assets/icons/arrow.svg";
-import Calendar from "@/assets/icons/calendar.svg";
-import Cn from "@/assets/icons/cn.svg";
-import Down from "@/assets/icons/down.svg";
-import Edit from "@/assets/icons/edit.svg";
-import Fab from "@/assets/icons/fab.svg";
-import Fr from "@/assets/icons/fr.svg";
-import Jp from "@/assets/icons/jp.svg";
-import Kr from "@/assets/icons/kr.svg";
-import Plus from "@/assets/icons/plus.svg";
-import Refresh from "@/assets/icons/refresh.svg";
-import Search from "@/assets/icons/search.svg";
-import Time from "@/assets/icons/time.svg";
-import Translate from "@/assets/icons/translate.svg";
+import EyeShow from '@/assets/icons/eye_show.svg';
+import EyeHide from '@/assets/icons/eye_hide.svg';
+import AI from '@/assets/icons/ai.svg';
+import Arrow from '@/assets/icons/arrow.svg';
+import Calendar from '@/assets/icons/calendar.svg';
+import Cn from '@/assets/icons/cn.svg';
+import Down from '@/assets/icons/down.svg';
+import Edit from '@/assets/icons/edit.svg';
+import Fab from '@/assets/icons/fab.svg';
+import Fr from '@/assets/icons/fr.svg';
+import Jp from '@/assets/icons/jp.svg';
+import Kr from '@/assets/icons/kr.svg';
+import Plus from '@/assets/icons/plus.svg';
+import Refresh from '@/assets/icons/refresh.svg';
+import Search from '@/assets/icons/search.svg';
+import Time from '@/assets/icons/time.svg';
+import Translate from '@/assets/icons/translate.svg';
 
 export default function Test() {
   return (
@@ -70,7 +70,7 @@ export default function Test() {
           <div className="h-6 w-6 bg-grayscale-700"></div>
           <div className="h-6 w-6 bg-grayscale-600"></div>
           <div className="h-6 w-6 bg-grayscale-500"></div>
-          <div className="h-6 w-6 bg-grayscale-600"></div>
+          <div className="h-6 w-6 bg-grayscale-400"></div>
           <div className="h-6 w-6 bg-grayscale-300"></div>
           <div className="h-6 w-6 bg-grayscale-200"></div>
           <div className="h-6 w-6 bg-grayscale-100"></div>
