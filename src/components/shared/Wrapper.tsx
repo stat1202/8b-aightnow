fccf72856c8afb060520d77e462ec73c06835ac8
BIROOM <zoon1031@naver.com>
2024-06-24 21:40:12 +0900

    feat #27 : wrapper 컴포넌트 구현
    - wrapper 컴포넌트 생성 및 구현
    - tailwind.config.ts button 및 shadow 커스텀 추가

---
+++ src/components/shared/Wrapper.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// padding값 string으로 받아 props로 받아 사용
+//  <Wrapper padding='p-48'> <div> 로그인 창</div> </Wrapper>
+// 기본값 py-[40px] px[56px]
+type WrapperProps = {
+  children: React.ReactNode;
+  padding?: string;
+};
+
+export default function Wrapper({ children, padding }: WrapperProps) {
+  const wrapperPadding = padding ? padding : 'py-[40px] px-[56px] ';
+  return (
+    <div
+      className={`border-none rounded-[32px] shadow-custom-xxs bg-grayscale-0 ${wrapperPadding}`}
+    >
+      {children}
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,114 +1,186 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     colors: {
       primary: {
-        50: "#E6E9EF",
-        100: "#C0C8D9",
-        200: "#98A5BF",
-        300: "#7282A5",
-        400: "#546894",
-        500: "#364F84",
-        600: "#30487C",
-        700: "#283E70",
-        800: "#213564",
-        900: "#18254C",
+        50: '#E6E9EF',
+        100: '#C0C8D9',
+        200: '#98A5BF',
+        300: '#7282A5',
+        400: '#546894',
+        500: '#364F84',
+        600: '#30487C',
+        700: '#283E70',
+        800: '#213564',
+        900: '#18254C',
       },
       secondary: {
-        50: "#E1F5FD",
-        100: "#B2E6FA",
-        200: "#80D5F7",
-        300: "#4CC5F4",
-        400: "#1CB8F3",
-        500: "#00ACF2",
-        600: "#009EE3",
-        700: "#008BD0",
-        800: "#0079BC",
-        900: "#005A9B",
+        50: '#E1F5FD',
+        100: '#B2E6FA',
+        200: '#80D5F7',
+        300: '#4CC5F4',
+        400: '#1CB8F3',
+        500: '#00ACF2',
+        600: '#009EE3',
+        700: '#008BD0',
+        800: '#0079BC',
+        900: '#005A9B',
       },
       grayscale: {
-        0: "#FFFFFF",
-        100: "#F5F5F5",
-        200: "#E9E9E9",
-        300: "#C5C5C5",
-        400: "#9F9F9F",
-        500: "#7D7D7D",
-        600: "#575757",
-        700: "#454545",
-        800: "#282828",
-        900: "#121212",
+        0: '#FFFFFF',
+        100: '#F5F5F5',
+        200: '#E9E9E9',
+        300: '#C5C5C5',
+        400: '#9F9F9F',
+        500: '#7D7D7D',
+        600: '#575757',
+        700: '#454545',
+        800: '#282828',
+        900: '#121212',
       },
       background: {
-        100: "#F1F3F8",
+        100: '#F1F3F8',
       },
       warning: {
-        100: "#FF294F",
+        100: '#FF294F',
       },
       success: {
-        100: "#1FCE65",
+        100: '#1FCE65',
       },
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom-xxs':
+          '0 0.25px 0.5px rgba(0, 0, 0, 0.15), 0 0 1px rgba(0, 0, 0, 0.15), 0 0 1px rgba(0, 0, 0, 0.1)',
+        'custom-xs':
+          '0 1px 2px rgba(0, 0, 0, 0.25), 0 0 3px rgba(0, 0, 0, 0.25), 0 0 3px rgba(0, 0, 0, 0.15)',
+        'custom-sm':
+          '0 2px 4px rgba(0, 0, 0, 0.25), 0 0 5px rgba(0, 0, 0, 0.25), 0 0 5px rgba(0, 0, 0, 0.15)',
+        'custom-md':
+          '0 3px 6px rgba(0, 0, 0, 0.25), 0 0 7px rgba(0, 0, 0, 0.25), 0 0 7px rgba(0, 0, 0, 0.15)',
+        'custom-lg':
+          '0 4px 8px rgba(0, 0, 0, 0.25), 0 0 10px rgba(0, 0, 0, 0.25), 0 0 10px rgba(0, 0, 0, 0.15)',
+      },
+    },
   },
   plugins: [
-    function ({ addUtilities }: { addUtilities: any }) {
+    function ({
+      addUtilities,
+      theme,
+    }: {
+      addUtilities: any;
+      theme: any;
+    }) {
       const newUtilities = {
-        ".h1": {
-          fontSize: "60px",
-          lineHeight: "72px",
-          letterSpacing: "-1%",
+        '.btn-primary': {
+          backgroundColor: theme('colors.primary.900'),
+          color: theme('colors.grayscale.0'),
         },
-        ".h2": {
-          fontSize: "48px",
-          lineHeight: "140%",
-          letterSpacing: "0%",
+        '.btn-primary:hover': {
+          backgroundColor: theme('colors.primary.700'),
         },
-        ".h3": {
-          fontSize: "36px",
-          lineHeight: "40px",
-          letterSpacing: "0%",
+        '.btn-light': {
+          backgroundColor: theme('colors.grayscale.0'),
+          color: theme('colors.primary.900'),
+          border: `1px solid ${theme('colors.primary.900')}`,
         },
-        ".h4": {
-          fontSize: "30px",
-          lineHeight: "36px",
-          letterSpacing: "0%",
+        '.btn-light:hover': {
+          color: theme('colors.primary.200'),
+          borderColor: theme('colors.primary.200'),
+        },
+        '.btn-danger': {
+          backgroundColor: theme('colors.warning.100'),
+          color: theme('colors.grayscale.0'),
+        },
+        '.btn-danger:hover': {
+          backgroundColor: theme('colors.warning.50'),
+        },
+        '.btn-success': {
+          backgroundColor: theme('colors.success.100'),
+          color: theme('colors.grayscale.0'),
+        },
+        '.btn-success:hover': {
+          backgroundColor: theme('colors.success.50'),
+        },
+        '.btn-blue': {
+          backgroundColor: theme('colors.secondary.500'),
+          color: theme('colors.grayscale.0'),
+        },
+        '.btn-blue:hover': {
+          backgroundColor: theme('colors.secondary.300'),
+        },
+        '.btn-gray': {
+          backgroundColor: theme('colors.grayscale.200'),
+          color: theme('colors.grayscale.600'),
+        },
+        '.btn-gray:hover': {
+          color: theme('colors.grayscale.500'),
+        },
+        '.btn-disabled': {
+          backgroundColor: theme('colors.grayscale.200'),
+          color: theme('colors.grayscale.400'),
+          cursor: 'not-allowed',
+          border: 'none',
+        },
+        '.btn-disabled:hover': {
+          border: 'none',
+        },
+        '.h1': {
+          fontSize: '60px',
+          lineHeight: '72px',
+          letterSpacing: '-1%',
+        },
+        '.h2': {
+          fontSize: '48px',
+          lineHeight: '140%',
+          letterSpacing: '0%',
+        },
+        '.h3': {
+          fontSize: '36px',
+          lineHeight: '40px',
+          letterSpacing: '0%',
+        },
+        '.h4': {
+          fontSize: '30px',
+          lineHeight: '36px',
+          letterSpacing: '0%',
         },
 
-        ".b1": {
-          fontSize: "24px",
-          lineHeight: "32px",
-          letterSpacing: "0%",
+        '.b1': {
+          fontSize: '24px',
+          lineHeight: '32px',
+          letterSpacing: '0%',
         },
-        ".b2": {
-          fontSize: "20px",
-          lineHeight: "28px",
-          letterSpacing: "0%",
+        '.b2': {
+          fontSize: '20px',
+          lineHeight: '28px',
+          letterSpacing: '0%',
         },
-        ".b3": {
-          fontSize: "18px",
-          lineHeight: "28px",
-          letterSpacing: "0%",
+        '.b3': {
+          fontSize: '18px',
+          lineHeight: '28px',
+          letterSpacing: '0%',
         },
-        ".b4": {
-          fontSize: "16px",
-          lineHeight: "24px",
-          letterSpacing: "0%",
+        '.b4': {
+          fontSize: '16px',
+          lineHeight: '24px',
+          letterSpacing: '0%',
         },
-        ".b5": {
-          fontSize: "14px",
-          lineHeight: "20px",
-          letterSpacing: "0%",
+        '.b5': {
+          fontSize: '14px',
+          lineHeight: '20px',
+          letterSpacing: '0%',
         },
-        ".caption": {
-          fontSize: "12px",
-          lineHeight: "16px",
-          letterSpacing: "0%",
+        '.caption': {
+          fontSize: '12px',
+          lineHeight: '16px',
+          letterSpacing: '0%',
         },
       };
 
