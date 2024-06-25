92912fba0982c9bde09711b663135b05fa7bda4f
unknown <gjstkd123@naver.com>
2024-06-25 16:13:05 +0900

    feat #49 : header zindex
    - Header Zindex 추가
    
    Close #49

--- src/app/(after)/layout.tsx
+++ src/app/(after)/layout.tsx
@@ -10,7 +10,7 @@ export default function AfterLayout({
     <div
       className={`w-full min-h-dvh bg-background-100 pt-32 pb-20 px-14 text-grayscale-900 flex justify-center`}
     >
-      <div className="w-full min-w-[1200px] h-[80px] flex items-center px-28 bg-[#FFFFFF] fixed top-0">
+      <div className="w-full min-w-[1200px] h-[80px] flex items-center px-28 bg-[#FFFFFF] fixed top-0 z-50">
         <Link href="/home">
           <Logo />
         </Link>
