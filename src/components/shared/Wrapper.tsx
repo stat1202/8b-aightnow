43d28686efffbe9554cbbd1b91bec2ea45dad8ab
BIROOM <zoon1031@naver.com>
2024-06-25 11:14:52 +0900

    refact 37# : Wrapper 컴포넌트 수정
    - Wrapper컴포넌트  스타일 코드 수정

--- src/components/shared/Wrapper.tsx
+++ src/components/shared/Wrapper.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 
-// padding값 string으로 받아 props로 받아 사용
-//  <Wrapper padding='p-48'> <div> 로그인 창</div> </Wrapper>
+// padding값 , width값 string으로 받아 props로 받아 사용
+//  <Wrapper padding='p-48' width="w-96"> <div> 로그인 창</div> </Wrapper>
 // 기본값 py-[40px] px[56px]
 type WrapperProps = {
   children: React.ReactNode;
   padding?: string;
+  width: string;
 };
 
-export default function Wrapper({ children, padding }: WrapperProps) {
+export default function Wrapper({
+  children,
+  padding,
+  width,
+}: WrapperProps) {
+  const wrapperWidth = width ? width : 'w-min';
   const wrapperPadding = padding ? padding : 'py-[40px] px-[56px]';
   return (
     <div
-      className={`border-none rounded-[32px] shadow-custom-xxs bg-grayscale-0 ${wrapperPadding}`}
+      className={`border-none rounded-[32px] shadow-md bg-grayscale-0 ${wrapperWidth} ${wrapperPadding}`}
     >
       {children}
     </div>
