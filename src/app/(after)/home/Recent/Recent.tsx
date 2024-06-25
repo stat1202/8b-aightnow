0c5b69360f679745084d8a65d914f4f003174ce9
unknown <gjstkd123@naver.com>
2024-06-25 18:33:06 +0900

    feat #52 : data가 null 일 경우 type 수정
    - Recent.tsx Props type수정

--- src/app/(after)/home/Recent/Recent.tsx
+++ src/app/(after)/home/Recent/Recent.tsx
@@ -9,13 +9,18 @@ type recentProps = {
   tmp2: number;
 };
 
-export default function Recent({ data }: { data: recentProps[] }) {
+export default function Recent({
+  data,
+}: {
+  data: recentProps[] | null;
+}) {
+  const datas = data;
   return (
     <>
       <div className="flex flex-col">
         <div className="h4 font-bold text-primary-900">최근 조회</div>
         <div className="flex flex-col justify-center items-center bg-[#FFFFFF] rounded-2xl mt-6 min-w-[590px] min-h-[374px] ">
-          {data ? <RecentItem stocks={data} /> : <NoData />}
+          {datas ? <RecentItem stocks={datas} /> : <NoData />}
         </div>
       </div>
     </>
