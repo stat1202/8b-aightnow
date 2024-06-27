2921bf57f36d4db2aed209cafb3bc4435d22ff93
stat1202 <hseungjae1202@naver.com>
2024-06-27 20:32:31 +0900

    feat #72 : 최신뉴스 - 무한스크롤 기능 추가
    - react-intersection-observer 패키지 설치
    - Recent News 컴포넌트 클라이언트 컴포넌트로 변경
    - 무한 스크롤 구현
    Close #72

--- src/components/news/RecentNews.tsx
+++ src/components/news/RecentNews.tsx
@@ -1,16 +1,25 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
 import NewsListItem, { News } from '../shared/NewsListItem';
+import { useInView } from 'react-intersection-observer';
+import { TMP_NEWS } from '@/constants';
 
-type RecentNewsProps = {
-  newsList: News[];
-};
+export default function RecentNews() {
+  const { ref, inView } = useInView();
+  const [newsList, setNewsList] = useState<News[]>([]);
 
-export default function RecentNews({ newsList }: RecentNewsProps) {
+  useEffect(() => {
+    if (inView) {
+      setNewsList((prev) => [...prev, TMP_NEWS, TMP_NEWS, TMP_NEWS]);
+    }
+  }, [inView]);
   return (
     <div className="bg-grayscale-0 p-12 flex flex-col rounded-2xl">
       {newsList.map((news) => (
         <NewsListItem type="large" news={news} key={news.id} />
       ))}
+      <span ref={ref}>end of contents</span>
     </div>
   );
 }
