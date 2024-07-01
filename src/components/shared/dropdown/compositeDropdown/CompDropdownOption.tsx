9fa819f0d7299c1c6c6af6f8afcc5a8bb621243a
seokhochoi <seokho9473@gmail.com>
2024-07-01 22:51:44 +0900

    feat #85: Option 컴포넌트 포커스 및 데이터셋 설계
    - 커스텀훅을 활용한 ref 포커스
    - data-value 를 사용해 이벤트 버블링 활용

---
+++ src/components/shared/dropdown/compositeDropdown/CompDropdownOption.tsx
@@ -0,0 +1,25 @@
+import { CompDropdownOptionProps } from './types';
+import { useFocusOption } from '@/hooks/dropdown/useFocusOption';
+
+export default function CompDropDownOption({
+  children,
+  className,
+  value,
+  selected = false,
+  index,
+  focusedIndex,
+}: CompDropdownOptionProps) {
+  const { focusedRef } = useFocusOption(focusedIndex, index);
+
+  return (
+    <li
+      ref={focusedRef}
+      className={`${className} ${selected ? 'bg-primary-50' : ''}`}
+      value={value}
+      data-value={value}
+      tabIndex={0}
+    >
+      {children}
+    </li>
+  );
+}
