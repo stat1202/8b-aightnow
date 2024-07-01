0ac75b64f16ea909f4eb85c29793f7fbee56f2fb
stat1202 <hseungjae1202@naver.com>
2024-07-01 15:09:18 +0900

    feat #93 : supabase 함수 추가 types 폴더 추가
    Close #93

--- next.config.mjs
+++ next.config.mjs
@@ -16,6 +16,12 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 's.yimg.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
---
+++ src/types/news.ts
@@ -0,0 +1,23 @@
+export type News = {
+  news_id: number;
+  published_at: string;
+  publisher: string;
+  thumbnail: string;
+  origin_url: string;
+  title_en: string;
+  content_en: string;
+  summary_en: string;
+  title_ko: string;
+  content_ko: string;
+  summary_ko: string;
+  title_fr: string;
+  content_fr: string;
+  summary_fr: string;
+  title_ja: string;
+  content_ja: string;
+  summary_ja: string;
+  title_zh: string;
+  content_zh: string;
+  summary_zh: string;
+  view: number;
+};
