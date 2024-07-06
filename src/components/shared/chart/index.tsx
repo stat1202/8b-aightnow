f6cecb695a3916122316cfa11572d4b3f192ae72
seokhochoi <seokho9473@gmail.com>
2024-07-06 19:41:52 +0900

    feat #110: Recharts 라이브러리 활용 및 합성 구현

---
+++ src/components/shared/chart/index.tsx
@@ -0,0 +1,9 @@
+import ChartMain from './ChartMain';
+import SpecificStockAIReport from './SpecificStockAIReport';
+import StockAIReportCard from './StockAIReportCard';
+
+export const Chart = Object.assign(ChartMain, {
+  SpecificStockAIReport,
+  StockAIReportCard,
+});
+export default Chart;
