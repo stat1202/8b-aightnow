e04595a01ef5bbfdec7ccda8865de4bafe84c458
seokhochoi <seokho9473@gmail.com>
2024-07-09 11:38:57 +0900

    feat #132: Infinite Scroll 라이브러리 적용
    - Virtual Scroll 적용 보류
    - 임시 API 요청

---
+++ src/components/stock/RequestWrapper.tsx
@@ -0,0 +1,70 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import AddInterest from './AddInterest';
+import ShowingInterest from './ShowingInterest';
+import { InView, useInView } from 'react-intersection-observer';
+
+export default function RequestWrapper() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [stocks, setStocks] = useState<Array<any>>([]);
+  const [stockQuantity, setStockQuantity] = useState({
+    page: 1,
+    size: 17,
+  });
+  const { page, size } = stockQuantity;
+
+  /**
+   * @description
+   *  - 임시 API 요청
+   *  - 소통 후 HTTP Client 적용 예정
+   */
+  function getInterest() {
+    if (!isLoading) {
+      setIsLoading(true);
+
+      try {
+        fetch(
+          `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${size}`,
+        )
+          .then((res) => res.json())
+          .then((stocksRes) => {
+            if (stocksRes.length !== 0) {
+              setStockQuantity((prev) => ({
+                ...prev,
+                page: page + 1,
+              }));
+              setStocks((prev) => [...prev, ...stocksRes]);
+            }
+            setIsLoading(false);
+          });
+      } catch (e) {
+        console.error(`Request: ${e}`);
+        setIsLoading(false);
+      }
+    }
+  }
+  /**
+   * - initialInView: inView 초기값
+   * - onChange: inView 상태가 변화할때 마다 실행되는 콜백함수
+   * - threshold: Target Element가 얼마나 보였을때 노출됐다 판단 비율
+   * - triggerOnce: Observer가 딱 한 번만 실행
+   */
+  const { ref } = useInView({
+    initialInView: true,
+    threshold: 0.1,
+    onChange(inView) {
+      if (inView) {
+        getInterest();
+      }
+    },
+  });
+
+  return (
+    <>
+      <AddInterest />
+      <ShowingInterest stocks={stocks} isLoading={isLoading} />
+      <span ref={ref} />
+    </>
+  );
+}
