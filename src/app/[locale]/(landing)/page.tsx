85d642a387653925b558d03d6d9e6094b406544e
stat1202 <hseungjae1202@naver.com>
2024-07-12 22:06:06 +0900

    feat #173 : 랜딩 페이지 다국어 기능 추가
    Close #173

--- src/app/[locale]/(landing)/page.tsx
+++ src/app/[locale]/(landing)/page.tsx
@@ -4,8 +4,10 @@ import Image from 'next/image';
 import ButtonBase from '@/components/shared/buttons/ButtonBase';
 import Mockup from '@/assets/landing/mockup.png';
 import Link from 'next/link';
+import { getTranslations } from 'next-intl/server';
 
-export default function Landing() {
+export default async function Landing() {
+  const t = await getTranslations('Landing');
   return (
     <main className="flex flex-col items-center justify-end h-screen">
       <Image
@@ -15,19 +17,14 @@ export default function Landing() {
         className="-z-10"
       />
       <div className="text-grayscale-0 flex flex-col items-center pb-14 pt-14">
-        <h1 className="h1 font-medium pb-6">
-          해외주식은 <span className="font-bold">아잇나우</span>와
-          함께!
-        </h1>
+        <h1 className="h1 font-medium pb-6">{t('title')}</h1>
         <div className="text-center h4 font-medium pb-14">
-          <span className="block">해외 주식 뉴스 실시간 번역과</span>
-          <span className="block">
-            AI 애널리스트가 알려주는 어려운 해외주식 리포트
-          </span>
+          <span className="block">{t('text_1')}</span>
+          <span className="block">{t('text_2')}</span>
         </div>
         <Link href="/login" className="b3 font-medium">
           <ButtonBase className="bg-primary-800 w-[386px] h-16 rounded-lg">
-            로그인
+            {t('login')}
           </ButtonBase>
         </Link>
       </div>
