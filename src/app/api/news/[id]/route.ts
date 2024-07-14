f36a02751164e7c4c156b7c8bb1bec48724c9540
stat1202 <hseungjae1202@naver.com>
2024-07-14 14:35:24 +0900

    refact #184 : news 조회수 IP 기준 추가
    Close #184

--- src/app/api/news/[id]/route.ts
+++ src/app/api/news/[id]/route.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@/utils/supabase/server';
+import dayjs from 'dayjs';
 
 export async function GET(
   req: Request,
@@ -20,20 +21,56 @@ export async function PATCH(
   req: Request,
   { params }: { params: { id: string } },
 ) {
+  const ip = req.headers.get('X-Forwarded-For');
   const id = params.id;
   const supabase = createClient();
+
+  const { data: ipLog } = await supabase
+    .from('news_iplog')
+    .select('viewed_at')
+    .eq('news_id', id)
+    .eq('ip', ip)
+    .maybeSingle();
+  const now = dayjs().utc();
+  // console.log(ipLog, id);
+  const dayDiff = now.diff(ipLog?.viewed_at, 'day');
+  console.log(dayDiff);
   const { data: news, error } = await supabase
     .from('news')
     .select('view')
     .eq('news_id', id)
     .maybeSingle();
-
-  const update_view = await supabase
-    .from('news')
-    .update({ view: news?.view + 1 })
-    .eq('news_id', id)
-    .select('view')
-    .single();
-  // console.log(update_view.error);
-  return Response.json({ data: update_view.data });
+  // 뉴스를 본 적이 없을 때
+  if (!ipLog) {
+    await supabase.from('news_iplog').insert({ news_id: id, ip });
+    const update_view = await supabase
+      .from('news')
+      .update({ view: news?.view + 1 })
+      .eq('news_id', id)
+      .select('view')
+      .single();
+    return Response.json({ message: 'ip 없을 때 조회수 갱신 완료' });
+  } else {
+    if (dayDiff > 0) {
+      const { error } = await supabase
+        .from('news_iplog')
+        .update({
+          news_id: id,
+          viewed_at: dayjs()
+            .utc()
+            .format('YYYY-MM-DDTHH:mm:ss.SSSS[+00:00]'),
+          ip,
+        })
+        .eq('news_id', id)
+        .eq('ip', ip);
+      console.log(error);
+      const update_view = await supabase
+        .from('news')
+        .update({ view: news?.view + 1 })
+        .eq('news_id', id)
+        .select('view')
+        .single();
+    }
+    return Response.json({ message: '조회수 갱신 완료' });
+  }
 }
