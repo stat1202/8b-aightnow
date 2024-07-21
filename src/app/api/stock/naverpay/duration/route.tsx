716a8666434ff495a09594775713c352a3aabe11
seokhochoi <seokho9473@gmail.com>
2024-07-21 12:49:01 +0900

    feat #235: 네이버페이 증권의 기간별 API Route 로직 구현 (1일 - 10년)

---
+++ src/app/api/stock/naverpay/duration/route.tsx
@@ -0,0 +1,17 @@
+import { processPay } from '@/utils/processPay';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const companies = url.searchParams.get('companies');
+  const amount = url.searchParams.get('amount');
+  const range = amount ? `&range=${amount}` : '';
+  const periodType = url.searchParams.get('unit');
+  const naverpayURL = `https://api.stock.naver.com/chart/foreign/item/${companies}?periodType=${periodType}${range}&stockExchangeType=NASDAQ`;
+  const payResponse = await fetch(naverpayURL);
+  const payBody = await payResponse.json();
+
+  const processed = processPay(payBody.priceInfos);
+
+  return NextResponse.json({ processed, amount, periodType });
+}
