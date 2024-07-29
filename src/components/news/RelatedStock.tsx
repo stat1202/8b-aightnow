e037987129921875f32d02e011e302eb7da9976f
stat1202 <hseungjae1202@naver.com>
2024-07-29 23:14:49 +0900

    feature #283 : 뉴스 관련 주식 없을 때 UI 추가

--- src/components/news/RelatedStock.tsx
+++ src/components/news/RelatedStock.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import StockListItem from '../shared/StockListItem';
 import { Stock } from '@/types/stock';
 import { businessAPI } from '@/service/apiInstance';
+import { getTranslations } from 'next-intl/server';
+import Exclamation from '@/assets/icons/exclamation.svg';
 
 type RelatedStockProps = {
   id: string;
@@ -10,13 +12,21 @@ type RelatedStockProps = {
 export default async function RelatedStock({
   id,
 }: RelatedStockProps) {
+  const t = await getTranslations('NewsDetail');
   const { stockList }: { stockList: Stock[] } =
     await businessAPI.getRelatedStock({ newsId: id });
   return (
     <div>
-      {stockList.map((stock) => (
-        <StockListItem stock={stock} key={stock.stock_id} />
-      ))}
+      {stockList.length > 0 ? (
+        stockList.map((stock) => (
+          <StockListItem stock={stock} key={stock.stock_id} />
+        ))
+      ) : (
+        <div className="flex flex-col b3 font-bold text-center text-primary-900 pt-5 items-center gap-4">
+          <Exclamation />
+          {t('stock_warning')}
+        </div>
+      )}
     </div>
   );
 }
